87ce4a58b97a5f97685f1ecafe281952daa1c9d4
rensilva5 <renzoasilva@gmail.com>
2022-10-16 19:48:33 -0400

    Quiz app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
-function App() {
+export default function App() {
+  const questions = [
+    {
+      questionText: `what's the capital of France?`,
+      answerOptions: [
+        { answerText: 'New York', isCorrect: false },
+        { answerText: 'London', isCorrect: false },
+        { answerText: 'Paris', isCorrect: true },
+        { answerText: 'Dublin', isCorrect: false },
+      ],
+    },
+    {
+      questionText: `Who's the CEO of Tesla`,
+      answerOptions: [
+        { answerText: 'Jeff Bezos', isCorrect: false },
+        { answerText: 'Elon Musk', isCorrect: true },
+        { answerText: 'Bill Gates', isCorrect: false },
+        { answerText: 'Tony Stark', isCorrect: false },
+      ],
+    },
+    {
+      questionText: `The iPhone was create by which company?`,
+      answerOptions: [
+        { answerText: 'Apple', isCorrect: true },
+        { answerText: 'Intel', isCorrect: false },
+        { answerText: 'Amazon', isCorrect: false },
+        { answerText: 'Microsoft', isCorrect: false },
+      ],
+    },
+    {
+      questionText: `How many Harry Potter are there?`,
+      answerOptions: [
+        { answerText: '1', isCorrect: false },
+        { answerText: '6', isCorrect: true },
+        { answerText: '6', isCorrect: false },
+        { answerText: '7', isCorrect: true },
+      ],
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {false ? (
+          <div className='score-section'>Your score 1 of {questions.lenght}</div>
+      ) : (
+          <>
+              <div className='question-section'>
+                  <div className='question-count'>
+                    <span>Question 1</span>/{questions.length}
+                  </div>
+      <div className='question-test'>{questions[0].questionText}</div>
+              </div>
+                <div className='answer-question'>
+                  {questions[0].answerOptions.map((answerOption) => (
+                    <button>{answerOption.answerText}</button>
+
+                  ))}
+              </div>
+          </>
+      )}
     </div>
   );
 }
-
-export default App;
