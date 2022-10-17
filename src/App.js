c276f68d375082b9cef3e74832039eeb854ee8d7
rensilva5 <renzoasilva@gmail.com>
2022-10-16 20:39:01 -0400

    App done, needs style

--- src/App.js
+++ src/App.js
@@ -44,7 +44,12 @@ export default function App() {
 
   const [showScore, setShowScore] = useState(false)
 
-  const handleAnswerButtonClick = () => {
+  const [score, setScore] = useState(0)
+
+  const handleAnswerButtonClick = (isCorrect) => {
+    if( isCorrect === true) {
+      setScore(score + 1)
+    }
     const nextQuestion = currentQuestion + 1
     if (nextQuestion < questions.length) {
       setCurrentQuestion(nextQuestion)
@@ -56,18 +61,18 @@ export default function App() {
   return (
     <div className="app">
       {showScore ? (
-          <div className='score-section'>Your score 1 of {questions.length}</div>
+          <div className='score-section'>Your scored {score} of {questions.length}</div>
       ) : (
           <>
               <div className='question-section'>
                   <div className='question-count'>
-                    <span>Question 1</span>/{questions.length}
+                    <span>Question {currentQuestion + 1}</span>/{questions.length}
                   </div>
       <div className='question-test'>{questions[currentQuestion].questionText}</div>
               </div>
                 <div className='answer-question'>
                   {questions[currentQuestion].answerOptions.map((answerOption) => (
-                    <button onClick={handleAnswerButtonClick} >{answerOption.answerText}</button>
+                    <button onClick={() => handleAnswerButtonClick(answerOption.isCorrect)} >{answerOption.answerText}</button>
 
                   ))}
               </div>
